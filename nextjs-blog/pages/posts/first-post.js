26560c19eebc079ac8c30d402af3f4c4d2ab93be
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2023-10-04 18:50:01 +0200

    worked on concepts like global styling of components and rendering

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,20 +1,24 @@
 import Link from "next/link";
 import Head from "next/head";
-import Script from 'next/script';
+// import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <>
-      <Head>
-        <title>First Post</title>
-        <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
-      </Head>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+          {/* <Script
+      // src="https://connect.facebook.net/en_US/sdk.js"
+      strategy="lazyOnload"
+      onLoad={() =>
+        console.log(`script loaded correctly, window.FB has been populated`)
+      }
+    /> */}
+        </Head>
+      </Layout>
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
